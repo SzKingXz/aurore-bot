943aeda7bfebe482bf6f98eba28ad8c3b14f21ce
SzKingXz <soyjuanc27@gmail.com>
2026-02-25 21:38:33 -0300

    Initial commit: AURORE Bot v4.0.0

--- deploy-to-github.bat
+++ deploy-to-github.bat
@@ -16,42 +16,24 @@ echo Git OK
 echo.
 
 set /p USER="Tu usuario GitHub: "
-
-if "!USER!"=="" (
-    echo Necesitas ingresar un usuario
-    pause
-    exit /b 1
-)
-
 set /p EMAIL="Tu email GitHub: "
 
-if "!EMAIL!"=="" (
-    echo Necesitas ingresar un email
-    pause
-    exit /b 1
-)
-
 echo.
-echo Usuario: !USER!
-echo Email: !EMAIL!
-echo.
-
 echo Configurando Git...
-git config user.name "!USER!"
-git config user.email "!EMAIL!"
+git config user.name %USER%
+git config user.email %EMAIL%
 
 echo Limpiando remotes...
 git remote remove origin >nul 2>&1
 
-echo Agregando URL...
-git remote add origin https://github.com/!USER!/aurore-bot.git
+echo Agregando repositorio...
+git remote add origin https://github.com/%USER%/aurore-bot.git
 
-echo.
 echo Agregando archivos...
 git add .
 
 echo Haciendo commit...
-git commit -m "Initial commit: AURORE Bot v4.0.0" >nul 2>&1
+git commit -m "Initial commit: AURORE Bot v4.0.0" 2>nul
 
 echo.
 echo Haciendo push a GitHub...
@@ -63,11 +45,12 @@ git push -u origin main
 if errorlevel 1 (
     echo.
     echo ERROR en push
+    echo Verifica que el usuario es correcto
     pause
     exit /b 1
 )
 
 echo.
-echo EXITO!
+echo EXITO! Tu repositorio esta actualizado.
 echo.
 pause
